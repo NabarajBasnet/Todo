3d5b48bbe378dbfacef737bf5d2eb8c0c96fc156
nabaraj basnet <nabarajbasnet2000@gmail.com>
2025-02-03 22:28:39 +0545

    Basic node js server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('Node js server');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log('Hello Node Js');
+});
+
+server.listen(5000, () => {
+    console.log(`Server listening on port ${5000}`)
+});
